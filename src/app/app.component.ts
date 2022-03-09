2d13c8c76106827bcc7da474cc2c0629e1b78160
akashSlated <ajindal1512@gmail.com>
2022-03-09 21:52:50 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [
+    { type: "server", name: "TestServer", content: "Just a test" },
+    { type: "blueprint", name: "TestServer1", content: "Just a test 1" },
+  ];
 
-  onAddServer() {
+  onServerAdded(event: { serverName: string; serverContent: string }) {
     this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      type: "server",
+      name: event.serverName,
+      content: event.serverContent,
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
     });
   }
 }
